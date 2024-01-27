e198a83396d1a7e032acf327d5ea277943a0df16
aLEnSchool <146203376+aLEnSchool@users.noreply.github.com>
2024-01-26 20:08:08 -0500

    Restricted Rotation

--- Assets/CanonController.cs
+++ Assets/CanonController.cs
@@ -4,30 +4,59 @@ using UnityEngine;
 
 public class CanonController : MonoBehaviour
 {
+    /* Rotation of Canon Variables */
+    
+    public float rotationSpeed;         //How fast barrel will rotate along z-axis
+
+    public bool rotateCanonUpwards;     //Allow rotation upwards
+    public bool rotateCanonDownwards;   //Allow rotation downwards
+
+    private float maxRotation = 43.5f;    //Restrict rotation upwards     (Max Value barrel may rotate)
+    private float minRotation = 300f;    //Restrict rotation downwards    (Min Value barrel may rotate)
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotationSpeed = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-               
+
+        Debug.Log(transform.eulerAngles.z);
+
+        if (rotateCanonUpwards)
+        {
+            transform.Rotate(0f, 0.0f, rotationSpeed, Space.World);
+        }
+        if (rotateCanonDownwards)
+        {
+            transform.Rotate(0f, 0.0f, -rotationSpeed, Space.World);
+        }
+
+        if (transform.eulerAngles.z >= maxRotation && transform.eulerAngles.z < 100f)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, maxRotation);
+        }
+        if (transform.eulerAngles.z <= minRotation && transform.eulerAngles.z > 100f)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, minRotation);
+        }
     }
 
     /* CANON BUTTON CONTROLS */
-    //Rotate Canon Upwards
-    public void rotateUp()
+    //Allow to rotate Canon Upwards
+    public void rotateUp(bool _rotateUp)
     {
         Debug.Log("Rotate Upwards");
-        transform.Rotate(0f, 0.0f, 10.0f, Space.World);
+        rotateCanonUpwards = _rotateUp;
     }
-    //Rotate Canon Downwards
-    public void rotateDown()
+    //Allow to rotate Canon Downwards
+    public void rotateDown(bool _rotateDown)
     {
         Debug.Log("Rotate Downwards");
-        transform.Rotate(0f, 0.0f, -10.0f, Space.World);
+        rotateCanonDownwards = _rotateDown;
     }
 
 }

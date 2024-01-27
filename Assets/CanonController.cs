20e603ace95d3da21b58bf8e97044d45481c5196
aLEnSchool <146203376+aLEnSchool@users.noreply.github.com>
2024-01-26 19:02:07 -0500

    Rotating Canon (Not by holding)

--- Assets/CanonController.cs
+++ Assets/CanonController.cs
@@ -13,6 +13,21 @@ public class CanonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+               
     }
+
+    /* CANON BUTTON CONTROLS */
+    //Rotate Canon Upwards
+    public void rotateUp()
+    {
+        Debug.Log("Rotate Upwards");
+        transform.Rotate(0f, 0.0f, 10.0f, Space.World);
+    }
+    //Rotate Canon Downwards
+    public void rotateDown()
+    {
+        Debug.Log("Rotate Downwards");
+        transform.Rotate(0f, 0.0f, -10.0f, Space.World);
+    }
+
 }
